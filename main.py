e57886e5f0ff310173030eacd74e7db799ae51ea
Evan Pozzo <pozzoevan54@gmail.com>
2024-10-24 18:25:28 -0500

    Initialize the project

---
+++ main.py
@@ -0,0 +1,12 @@
+# Author: Evan Pozzo
+# Date: 10/25/2024
+# Purpose: COMP_SCI-5300-101, Programming Project: RDBMS Normalizer
+
+def main():
+    # take input from .xslx file
+
+    # loop over each table -> will parse them one at a time
+    tableName = "" 
+    columnArray, keyConstraintsArray, multiValueAttrArray = []
+   
+    return
